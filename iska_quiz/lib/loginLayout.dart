1b51bbab596d607e138077fea93771f084639493
Wannes Van Regenmortel <wannesvr@hotmail.com>
2019-05-01 13:31:31 +0200

    Add single scrollview to Container so keyboard doesn't cause overflow

--- iska_quiz/lib/loginLayout.dart
+++ iska_quiz/lib/loginLayout.dart
@@ -27,42 +27,44 @@ class LoginLayout extends StatelessWidget {
           centerTitle: true,
         ),
         body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(60.0),
-                child: new IskaLogo(),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: Text(Strings.hypeText,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16)),
-              ),
-              Container(
-                child: new TextField(
-                  controller: this.userNameController,
-                  decoration: new InputDecoration(hintText: 'Username'),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(60.0),
+                  child: new IskaLogo(),
                 ),
-                width: 250,
-                margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-              ),
-              Container(
-                  child: Text(this.loginPageState.errorMessage,
-                      style: new TextStyle(color: Colors.red)),
-                  width: 200,
-                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10)),
-              Container(
-                padding: EdgeInsets.only(
-                    left: 60.0, top: 50.0, right: 60.0, bottom: 60.0),
-                child: IskaButton(
-                    onPressed: _handleStart,
-                    text: Strings.startQuiz,
-                    width: 250,
-                    height: 70),
-              ),
-            ],
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(Strings.hypeText,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16)),
+                ),
+                Container(
+                  child: new TextField(
+                    controller: this.userNameController,
+                    decoration: new InputDecoration(hintText: 'Username'),
+                  ),
+                  width: 250,
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                ),
+                Container(
+                    child: Text(this.loginPageState.errorMessage,
+                        style: new TextStyle(color: Colors.red)),
+                    width: 200,
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 10)),
+                Container(
+                  padding: EdgeInsets.only(
+                      left: 60.0, top: 50.0, right: 60.0, bottom: 60.0),
+                  child: IskaButton(
+                      onPressed: _handleStart,
+                      text: Strings.startQuiz,
+                      width: 250,
+                      height: 70),
+                ),
+              ],
+            ),
           ),
         ));
   }
