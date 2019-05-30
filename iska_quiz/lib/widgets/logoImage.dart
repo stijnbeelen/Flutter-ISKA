f0b4c6f7cbbd66f5cfc8bb1b2894d53015deb7d6
Wannes Van Regenmortel <wannesvr@hotmail.com>
2019-05-30 20:09:55 +0200

    Move everything up one folder

--- iska_quiz/lib/widgets/logoImage.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class IskaLogo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Image(image: AssetImage("lib/img/is_logo.png"));
-  }
-}
